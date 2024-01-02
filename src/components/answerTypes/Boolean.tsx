9362561f1e800090c010f799d2e5200ec3eeb483
Yaseer Arfat <ansariyaseer786@gmail.com>
2024-01-02 20:49:23 +0530

    Boolean type answer ui done

--- src/components/answerTypes/Boolean.tsx
+++ src/components/answerTypes/Boolean.tsx
@@ -1,14 +1,38 @@
-import { Divider } from "@nextui-org/react";
-import React from "react";
+import { Divider, Switch } from "@nextui-org/react";
+import { MoonIcon, SunIcon } from "lucide-react";
+import React, { useState } from "react";
+import Style from "./booleanStyle.module.css";
 
 const Boolean = () => {
+  const [isChecked, setIsChecked] = useState(true);
   return (
     <div>
       <h5 className="text-center pt-2 text-base font-medium text-blue-800">
-        Answer type: Yes/No
+        Answer type: Yes / No
       </h5>
       <div className="mx-4 my-2">
         <Divider />
+      </div>
+
+      <div className="p-4 flex justify-center">
+        <div className={`${Style.radio_inputs} gap-2`}>
+          <label className={`${Style.radio} `}>
+            <input
+              type="radio"
+              checked={isChecked}
+              onChange={() => setIsChecked(true)}
+            />
+            <span className={`${Style.name}`}>NO</span>
+          </label>
+          <label className={`${Style.radio} `}>
+            <input
+              type="radio"
+              checked={!isChecked}
+              onChange={() => setIsChecked(false)}
+            />
+            <span className={`${Style.name}`}>Yes</span>
+          </label>
+        </div>
       </div>
     </div>
   );
